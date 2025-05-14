c3741dfa69a5e2d64683a1e8da838532a7105642
Julius Schneller <julius.schneller@stud-mail.uni-wuerzburg.de>
2025-05-14 17:42:56 +0100

    added exploration

--- run_simulation.py
+++ run_simulation.py
@@ -8,11 +8,11 @@ from policy import LearnedMiniMaxPolicy
 from value_function import MinimaxQ_Function
 
 env = Environment()
-policy_A = LearnedMiniMaxPolicy(env, 0)
+policy_A = LearnedMiniMaxPolicy(env, 0, 0.1)
 value_function_A = MinimaxQ_Function(policy_A)
 agent_A = Agent(env, value_function_A, 0)
 
-policy_B = LearnedMiniMaxPolicy(env, 1)
+policy_B = LearnedMiniMaxPolicy(env, 1, 0.1)
 value_function_B = MinimaxQ_Function(policy_B)
 agent_B = Agent(env, value_function_B, 1)
 Simulation(env, agent_A, agent_B, True, False).run()
