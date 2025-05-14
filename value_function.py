489ac1c653c4496c8fb810e83145b1889a21d0e2
Julius Schneller <julius.schneller@stud-mail.uni-wuerzburg.de>
2025-05-14 16:50:47 +0100

    changed to tuples as keys

--- value_function.py
+++ value_function.py
@@ -1,6 +1,12 @@
 from collections import defaultdict
 from policy import *
 
+def state_to_tuple(state: list) -> tuple:
+    """
+    Converts a state from a list to a tuple.
+    """
+    return tuple((state[0][0], state[0][1]), (state[1][0], state[1][1]), state[2])
+
 
 '''Missing: Policy for minimaxQ_Function and general Policy -> i have to look at this first'''
 
@@ -48,18 +54,22 @@ class Q_Function(Value_Function):
         """
         Returns the state-value of a given state.
         """
+        state = state_to_tuple(state)
         return max(self.Q[state].values())
 
     def getQValue(self, state:list, action: str, action_opponent: str) -> float:
         """
         returns the state-action-value or Q-Value of a given state-action pair.
         """
+        state = state_to_tuple(state)
         return self.Q[state][action]
     
     def updateQValue(self, state: list, future_state: list, action: str, possible_actions: list, possible_opponent_actions: list, reward: int):
         """
         Updates the Q-value of a given state-action pair.
         """
+        state = state_to_tuple(state)
+        future_state = state_to_tuple(future_state)
         self.Q[state][action] = (1 - self.learning_rate) * self.Q[state][action] +\
                                  self.learning_rate * (reward + self.discount_factor * self.getValue(future_state))
 
@@ -92,6 +102,7 @@ class MinimaxQ_Function(Value_Function):
         """
         Returns the state-value of a given state.
         """
+        state = state_to_tuple(state)
         if state not in self.V:
             return self.start_value
         
@@ -102,12 +113,15 @@ class MinimaxQ_Function(Value_Function):
         """
         returns the state-action-value or Q-Value of a given state-action pair.
         """
+        state = state_to_tuple(state)
         return self.Q[state][action][action_opponent]
     
     def updateQValue(self, state: list, future_state: list, action: str, action_opponent: str, possible_actions: list, possible_opponent_actions: list, reward: int):
         """
         Updates the Q-value of a given state-action pair.
         """
+        state = state_to_tuple(state)
+        future_state = state_to_tuple(future_state)
         self.Q[state][action][action_opponent] = (1 - self.learning_rate) * self.Q[state][action][action_opponent] + self.learning_rate * (reward + self.discount_factor * self.getValue(state))
         self.learning_rate = self.learning_rate * self.discount_factor
         
